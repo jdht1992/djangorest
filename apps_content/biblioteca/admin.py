29d8551dda90ead12b4743799aae9ad5f7e6de8b
Diego Herrera <jdht1992@gmail.com>
2019-03-06 18:57:19 -0600

    personalizando el admin django

--- apps_content/biblioteca/admin.py
+++ apps_content/biblioteca/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from apps_content.biblioteca.models import Store, University, Book, Author, Publisher, Student, Loan
 
-admin.site.register(Store)
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'direction', 'post_books']
+    list_filter = ('name', 'direction')
+    search_fields = ('name', 'direction',)
+    readonly_fields = ('created', 'modified')
+    date_hierarchy = 'created'
+
+    def post_books(self, obj):
+        return ", ".join([c.name for c in obj.books.all().order_by("name")])
+    post_books.short_description = "Books"
+
+
+admin.site.register(Store, StoreAdmin)
 admin.site.register(University)
 admin.site.register(Book)
 admin.site.register(Author)
